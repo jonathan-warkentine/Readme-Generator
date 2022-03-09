7d3d0f5bca8676088c64751df8df37a0a0243287
Jonathan Warkentine <jonathan.warkentine@gmail.com>
2022-03-09 16:19:16 -0500

    moved questions array to its own file in new folder 'lib'

--- index.js
+++ index.js
@@ -2,57 +2,8 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const stringify = require('querystring');
 const generateMarkdown = require('./utils/generateMarkdown');
+const { questions } = require('./lib/questions')
 
-
-
-const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: "Project TITLE?:",
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Project DESCRIPTION?:'
-    },
-    {
-        type: 'input',
-        name: 'installInstr',
-        message: "Installation Instructions?:",
-    },
-    {
-        type: 'input',
-        name: 'usageInfo',
-        message:  "Usage Information?:",
-    },
-    {
-        type: 'input',
-        name: 'contrGuidelines',
-        message:  "Contribution guidelines:",
-    },
-    {
-        type: 'input',
-        name: 'testingInstructions',
-        message: 'Test Instructions?:',
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message:   "Project LICENSE?:",
-        choices: ['MIT License', 'Apache 2.0 License', 'GNU GPL v3', "None"],
-    },
-    {
-        type: 'input',
-        name: 'githubUsername',
-        message: "What is your GitHub USERNAME?:",
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is a good email address for questions about this project?:'
-    }
-];
 
 function writeToFile(projectTitle, data) {
     if (!fs.existsSync(`./dist/${projectTitle}`)){ //make sure we have a folder with the project name to save our resulting README to
@@ -81,9 +32,9 @@ function init() {
         })
     }
 
-    inquirer.prompt( questions).then((answers => {
+    inquirer.prompt( questions).then(answers => {
         writeToFile(answers.title, generateMarkdown(answers));
-    }))
+    })
 }
 
 init();
